ca098d4a998ff8c931f8ecb71eae03f503721a63
Krzysztof Urban <k.urban@g.pl>
2020-03-24 19:33:39 +0100

    Add project structure

---
+++ src/main/java/com/example/springsoapexample/SpringSoapExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsoapexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoapExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSoapExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsoapexample/SpringSoapExampleApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.springsoapexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoapExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
